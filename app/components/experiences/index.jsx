db023d57a000b94746fa54b03f0a47e7eb7b6bcb
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-05-11 17:45:03 +0600

    complete experience section

--- app/components/experiences/index.jsx
+++ app/components/experiences/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionTitle from "../helper/section-title";
+import Link from "next/link";
 
 const Experiences = () => {
   return (
@@ -15,64 +16,58 @@ const Experiences = () => {
           </h3>
           <div className="content">
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>High School Degree</h3>
+              <h2>Bachelor Degree</h2>
+              <h4>2021 - Present</h4>
+              <h3>Shahid Bulbul Government College, Pabna</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              At Shahid Bulbul Government College, Pabna, I am pursuing a Bachelor's degree in Political Science, immersing myself in a comprehensive academic program that nurtures critical thinking and civic engagement. This institution equips me with the knowledge and skills to contribute meaningfully to society.
               </p>
             </div>
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>Bachelors Degree</h3>
+              <h2>Inter Mediate Degree</h2>
+              <h4>2019 - 2021</h4>
+              <h3>Hazi Jasim Uddin Degree(Hons) College, Pabna</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              At Hazi Jasim Uddin Degree College, Pabna, I thrived in an enriching academic atmosphere, supported by dedicated faculty and diverse opportunities.
               </p>
             </div>
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>Bachelors Degree</h3>
+              <h2>High School Degree</h2>
+              <h4>2016 - 2019</h4>
+              <h3>Dublia High School, Pabna</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              Dublia High School provided a dynamic learning environment where I cultivated my academic skills and personal growth. Its rigorous curriculum and supportive community.
               </p>
             </div>
           </div>
         </div>
         <div>
           <h3 className="text-3xl text-center py-4 font-semibold uppercase bg-gradient-to-r from-primary-icon to-primary-title inline-block text-transparent bg-clip-text">
-            Experience
+          Learning & Experience
           </h3>
           <div className="content">
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>High School Degree</h3>
+              <h2><Link href="https://ultradevs.com/" target="_blank" >ultraDevs</Link> - Pabna, Bangladesh</h2>
+              <h4>2023 - Present</h4>
+              <h3>Full Stack Developer(Full-time)</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              As a full stack developer at ultraDevs since 2023, I've contributed to diverse projects, leveraging my skills in both frontend and backend development. Collaborating with talented teams, I've tackled challenges, implemented innovative solutions, and continuously honed my expertise to deliver high-quality, scalable software solutions.
               </p>
             </div>
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>Bachelors Degree</h3>
+              <h2>Programming Hero</h2>
+              <h4>2022 - 2023</h4>
+              <h3>Next Level Web Development</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              I completed the 'Next Level Web Development Course' from Programming Hero, guided by interactive lessons and real world projects, solidifying my expertise in web development.
               </p>
             </div>
             <div className="box">
-              <h4>2018 - 2022</h4>
-              <h3>Bachelors Degree</h3>
+              <h2>Self Learning</h2>
+              <h4>2020 - 2022</h4>
+              <h3>Youtube and W3Schools</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
+              I acquired programming proficiency through self-study, leveraging instructional resources from YouTube and W3Schools to expand my skill set and pursue my passion for coding
               </p>
             </div>
           </div>
